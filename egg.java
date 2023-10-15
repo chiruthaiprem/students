6629137538a58b4dbc946a91665d12396da2aeb6
chiruthaiprem <rprem555125@gmail.comgit config --global user.email rprem555125@gmail.com>
2023-10-15 21:19:27 +0530

    che

---
+++ egg.java
@@ -0,0 +1,9 @@
+public class cokking {
+	public static void main(String args[]){
+		
+	  double eggRate = 6.5;
+		double needEgg = 5;
+		
+		double totalAmount = eggRate*needEgg;
+		
+		System.out.printn( "Amount is " + totalAmount);
