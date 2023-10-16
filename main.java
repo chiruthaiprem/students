5e6bb26adf77e0ca9ba5da3eb21b20bdedeb5cbe
chiruthaiprem <rprem555125@gmail.comgit config --global user.email rprem555125@gmail.com>
2023-10-16 18:38:20 +0530

    upMain 2

--- main.java
+++ main.java
@@ -2,5 +2,16 @@ public class Main {
 	public static void main (String args []){
 		
 		System.out.println("  this is pratice ");
+		
+		
+	    int a = 10;
+		int b = 15;
+		
+		int c = a + b;
+		
+		System.out.println(a+b);
+		
 	}
 }
+
+
